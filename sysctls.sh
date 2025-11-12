591727394b5e71ce9194d45599e12476967e5b48
Vidal Alvarez Manuel <valvarezma@unsa.edu.pe>
2025-11-12 12:37:54 -0500

    systemctls

---
+++ sysctls.sh
@@ -0,0 +1,12 @@
+# en la instancia EC2 (bash)
+sudo tee /etc/sysctl.d/99-wireguard.conf > /dev/null <<'EOF'
+net.ipv4.ip_forward=1
+net.core.rmem_max=16777216
+net.core.wmem_max=16777216
+net.core.netdev_max_backlog=5000
+net.netfilter.nf_conntrack_max=262144
+net.netfilter.nf_conntrack_udp_timeout=30
+EOF
+
+sudo sysctl --system
+sudo docker compose up -d wireguard
